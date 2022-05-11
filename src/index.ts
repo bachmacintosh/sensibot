e9e42d52370a021feaf65bf2b341f93be5461306
Collin Bachman <collin@bachman.io>
2022-05-11 03:04:54 -0400

    Strictly check types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import {RESTPostAPIChannelMessageJSONBody} from "discord-api-types/v10";
+
+type Env = {
+  DISCORD_BOT_TOKEN: string;
+  DISCORD_CHANNEL_ID: string;
+  SENSIBO_API_KEY: string;
+  SENSIBO_DEVICE_ID: string;
+  TOMORROW_LOCATION_ID: string;
+  TOMORROW_API_KEY: string;
+};
 type Interval = {
   startTime: string;
   values: {
@@ -47,26 +57,26 @@ type SensiboResponse = {
 };
 
 export default {
-  fetch(request, env, context) {
+  fetch(request: Request, env: Env, context: ExecutionContext) {
     return new Response("Hello");
   },
-  async scheduled(event, env, ctx) {
-    ctx.waitUntil(handleScheduled(event));
+  async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
+    ctx.waitUntil(handleScheduled(event, env));
   },
 };
 
-async function handleScheduled(event) {
-  const tomorrow = await fetchTomorrow();
-  const sensibo = await fetchSensibo();
+async function handleScheduled(event: ScheduledEvent, env: Env) {
+  const tomorrow = await fetchTomorrow(env);
+  const sensibo = await fetchSensibo(env);
   console.log("Temps");
   console.log(JSON.stringify(tomorrow, null, 2));
   console.log("Sensibo");
   console.log(JSON.stringify(sensibo, null, 2));
-  await handleAcState(event, tomorrow, sensibo);
+  await handleAcState(env, event, tomorrow, sensibo);
 }
 
-async function fetchTomorrow() {
-  const url = `https://api.tomorrow.io/v4/timelines?location=${TOMORROW_LOCATION_ID}&fields=temperature,temperatureApparent&units=imperial&timesteps=current`;
+async function fetchTomorrow(env: Env) {
+  const url = `https://api.tomorrow.io/v4/timelines?location=${env.TOMORROW_LOCATION_ID}&fields=temperature,temperatureApparent&units=imperial&timesteps=current`;
   const headers = new Headers({
     apiKey: TOMORROW_API_KEY
   });
@@ -77,13 +87,13 @@ async function fetchTomorrow() {
   return await response.json() as TomorrowResponse;
 }
 
-async function fetchSensibo() {
-  const url = `https://home.sensibo.com/api/v2/pods/${SENSIBO_DEVICE_ID}?apiKey=${SENSIBO_API_KEY}&fields=acState,measurements`;
+async function fetchSensibo(env: Env) {
+  const url = `https://home.sensibo.com/api/v2/pods/${env.SENSIBO_DEVICE_ID}?apiKey=${env.SENSIBO_API_KEY}&fields=acState,measurements`;
   const response = await fetch(url);
   return await response.json() as SensiboResponse;
 }
 
-async function handleAcState(event, tomorrow: TomorrowResponse, sensibo: SensiboResponse) {
+async function handleAcState(env: Env, event: ScheduledEvent, tomorrow: TomorrowResponse, sensibo: SensiboResponse) {
   const dateOptions = {timeZone: "America/New_York"};
   const date = new Date().toLocaleString("en-US", dateOptions);
   const runTime = new Date(date).getHours();
@@ -93,61 +103,61 @@ async function handleAcState(event, tomorrow: TomorrowResponse, sensibo: Sensibo
   if (sensibo.result.acState.on) {
     if (sensibo.result.acState.mode === "cool" || sensibo.result.acState.mode === "dry") {
       if (outdoorTemp < 60 && roomTemp < 75) {
-        await turnAcOff(sensibo);
-        await sendToDiscord(true, `Outdoor Temp ${outdoorTemp}°F < 60°F`, true, "Fan", 70);
+        await turnAcOff(env, sensibo);
+        await sendToDiscord(env, true, `Outdoor Temp ${outdoorTemp}°F < 60°F`, true, "Fan", 70);
       } else if (runTime < 8) {
-        await turnAcOff(sensibo);
-        await sendToDiscord(true, "Time is after 12:00AM", true, "Fan", 70);
+        await turnAcOff(env, sensibo);
+        await sendToDiscord(env, true, "Time is after 12:00AM", true, "Fan", 70);
       } else {
-        await sendToDiscord(false, `Outdoor Temp ${outdoorTemp}°F and/or Room Temp. ${roomTemp}°F are still warm`, true, "Fan", 70);
+        await sendToDiscord(env, false, `Outdoor Temp ${outdoorTemp}°F and/or Room Temp. ${roomTemp}°F are still warm`, true, "Fan", 70);
       }
     }
     if (sensibo.result.acState.mode === "fan") {
       if ((outdoorTemp >= 60 && outdoorTemp < 122) && roomTemp >= 75) {
         if (runTime >= 8) {
-          await turnAcOn("cool", 70);
-          await sendToDiscord(true, `Outdoor Temp ${outdoorTemp}°F >= 60°F, Room Temp >= ${roomTemp}°F`, true, "Cool", 70);
+          await turnAcOn(env, "cool", 70);
+          await sendToDiscord(env, true, `Outdoor Temp ${outdoorTemp}°F >= 60°F, Room Temp >= ${roomTemp}°F`, true, "Cool", 70);
         } else {
-          await sendToDiscord(false, `Outdoor Temp ${outdoorTemp}°F >= 60°F, Room Temp >= ${roomTemp}°F, but deferring Cool Mode until 8:00AM`, true, "Cool", 70);
+          await sendToDiscord(env, false, `Outdoor Temp ${outdoorTemp}°F >= 60°F, Room Temp >= ${roomTemp}°F, but deferring Cool Mode until 8:00AM`, true, "Cool", 70);
         }
       }
     }
     if (sensibo.result.acState.mode === "heat") {
       if (outdoorTemp < -22) {
-        await turnAcOff(sensibo);
-        await sendToDiscord(true, `Outdoor Temp ${outdoorTemp}°F < -22°F`, false, "Heat", 65);
+        await turnAcOff(env, sensibo);
+        await sendToDiscord(env, true, `Outdoor Temp ${outdoorTemp}°F < -22°F`, false, "Heat", 65);
       } else if (runTime >= 22 || runTime < 8) {
-        await sendToDiscord(false, "Heat Mode will stay on between 10:00PM-8:00AM", true, "Heat", 65);
+        await sendToDiscord(env, false, "Heat Mode will stay on between 10:00PM-8:00AM", true, "Heat", 65);
       } else if (roomTemp >= 75) {
-        await turnAcOff(sensibo);
-        await sendToDiscord(true, `Room Temp ${roomTemp}°F >= 75°F`, false, "Heat", 65);
+        await turnAcOff(env, sensibo);
+        await sendToDiscord(env, true, `Room Temp ${roomTemp}°F >= 75°F`, false, "Heat", 65);
       } else if (outdoorTemp >= 50) {
-        await turnAcOff(sensibo);
-        await sendToDiscord(true, `Outdoor Temp ${outdoorTemp}°F >= 50°F`, false, "Heat", 65);
+        await turnAcOff(env, sensibo);
+        await sendToDiscord(env, true, `Outdoor Temp ${outdoorTemp}°F >= 50°F`, false, "Heat", 65);
       } else {
-        await sendToDiscord(false, `Outdoor Temp ${outdoorTemp}°F and/or Room Temp. ${roomTemp}°F are still cold`, true, "Heat", 65);
+        await sendToDiscord(env, false, `Outdoor Temp ${outdoorTemp}°F and/or Room Temp. ${roomTemp}°F are still cold`, true, "Heat", 65);
       }
     }
   } else if (!sensibo.result.acState.on && sensibo.result.acState.mode === "heat") {
     if ((runTime >= 22 || runTime < 8) && outdoorTemp > -22 && outdoorTemp < 35) {
-      await turnAcOn("heat", 65);
-      await sendToDiscord(true, `Outdoor Temp ${outdoorTemp}°F < 35°F tonight`, true, "Heat", 65);
+      await turnAcOn(env, "heat", 65);
+      await sendToDiscord(env, true, `Outdoor Temp ${outdoorTemp}°F < 35°F tonight`, true, "Heat", 65);
     } else if (roomTemp < 70 && outdoorTemp > -22 && outdoorTemp < 35) {
-      await turnAcOn("heat", 65);
-      await sendToDiscord(true, `Outdoor Temp ${outdoorTemp}°F <= 35°F`, true, "Heat", 65);
+      await turnAcOn(env, "heat", 65);
+      await sendToDiscord(env, true, `Outdoor Temp ${outdoorTemp}°F <= 35°F`, true, "Heat", 65);
     } else if (roomTemp < 65 && outdoorTemp > -22 && outdoorTemp < 86) {
-      await turnAcOn("heat", 65);
-      await sendToDiscord(true, `Room Temp ${roomTemp}°F <= 65°F`, true, "Heat", 65);
+      await turnAcOn(env, "heat", 65);
+      await sendToDiscord(env, true, `Room Temp ${roomTemp}°F <= 65°F`, true, "Heat", 65);
     } else {
-      await sendToDiscord(false, `Outdoor Temp ${outdoorTemp}°F and/or Room Temp. ${roomTemp}°F are still warm`, true, "Heat", 65);
+      await sendToDiscord(env, false, `Outdoor Temp ${outdoorTemp}°F and/or Room Temp. ${roomTemp}°F are still warm`, true, "Heat", 65);
     }
   } else {
-    await sendToDiscord(false, "A/C is off, no action taken", false, "Cool", 70);
+    await sendToDiscord(env, false, "A/C is off, no action taken", false, "Cool", 70);
   }
 }
 
-async function turnAcOn(mode, temp) {
-  const url = `https://home.sensibo.com/api/v2/pods/${SENSIBO_DEVICE_ID}/acStates?apiKey=${SENSIBO_API_KEY}`;
+async function turnAcOn(env: Env, mode: string, temp: number) {
+  const url = `https://home.sensibo.com/api/v2/pods/${env.SENSIBO_DEVICE_ID}/acStates?apiKey=${env.SENSIBO_API_KEY}`;
   const headers = new Headers({"Content-Type": "application/json"});
   const body = JSON.stringify({
     acState: {
@@ -167,9 +177,9 @@ async function turnAcOn(mode, temp) {
   await fetch(url, init);
 }
 
-async function turnAcOff(sensibo) {
+async function turnAcOff(env: Env, sensibo: SensiboResponse) {
   let body;
-  if (sensibo.acState.mode === "cool" || sensibo.acState.mode === "dry") {
+  if (sensibo.result.acState.mode === "cool" || sensibo.result.acState.mode === "dry") {
     body = JSON.stringify({
       acState: {
         on: true,
@@ -177,7 +187,7 @@ async function turnAcOff(sensibo) {
         fanLevel: "auto"
       }
     });
-  } else if (sensibo.acState.mode === "heat") {
+  } else if (sensibo.result.acState.mode === "heat") {
     body = JSON.stringify({
       acState: {
         on: false,
@@ -186,7 +196,7 @@ async function turnAcOff(sensibo) {
       }
     });
   }
-  const url = `https://home.sensibo.com/api/v2/pods/${SENSIBO_DEVICE_ID}/acStates?apiKey=${SENSIBO_API_KEY}`;
+  const url = `https://home.sensibo.com/api/v2/pods/${env.SENSIBO_DEVICE_ID}/acStates?apiKey=${env.SENSIBO_API_KEY}`;
   const headers = new Headers({"Content-Type": "application/json"});
   const init = {
     method: "POST",
@@ -197,52 +207,53 @@ async function turnAcOff(sensibo) {
   await fetch(url, init);
 }
 
-async function sendToDiscord(updated, reason, power, mode, temp) {
+async function sendToDiscord(env: Env, updated: boolean, reason: string, power: boolean, mode: string, temp: number) {
   const url = `https://discord.com/api/v9/channels/${DISCORD_CHANNEL_ID}/messages`;
   const headers = new Headers({
     Authorization: `Bot ${DISCORD_BOT_TOKEN}`,
     "Content-Type": "application/json"
   });
-  const payload = {
-    embeds: [{
-      title: updated ? "A/C State Updated" : "A/C State Not Updated",
-      description: updated ? "The state of the A/C was changed to the following settings:" : "The A/C State was not updated for the following reason:",
-      color: 3195298,
-      fields: [],
-    }]
-  };
-  if (updated === false) {
-    payload.embeds[0].fields.push({
+  const fields = [];
+  if (!updated) {
+    fields.push({
       name: "Reason",
       value: reason,
       inline: true
     });
   } else {
-    payload.embeds[0].fields.push({
+    fields.push({
       name: "Power",
       value: power ? "On" : "Off",
       inline: true
     });
-    if (power === true) {
-      payload.embeds[0].fields.push({
+    if (power) {
+      fields.push({
         name: "Mode",
         value: mode,
         inline: true
       });
       if (mode !== "Fan") {
-        payload.embeds[0].fields.push({
+        fields.push({
           name: "Temp",
           value: `${temp}°F`,
           inline: true
         });
       }
     }
-    payload.embeds[0].fields.push({
+    fields.push({
       name: "Reason",
       value: reason,
       inline: true
     });
   }
+  const payload: RESTPostAPIChannelMessageJSONBody = {
+    embeds: [{
+      title: updated ? "A/C State Updated" : "A/C State Not Updated",
+      description: updated ? "The state of the A/C was changed to the following settings:" : "The A/C State was not updated for the following reason:",
+      color: 3195298,
+      fields,
+    }]
+  };
 
   const init = {
     method: "POST",
