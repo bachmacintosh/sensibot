6f8cce50c1d9e78ca0394fab2ee361bcd0890619
Collin Bachman <collin@bachman.io>
2022-05-18 19:09:01 -0400

    Handle Discord PING

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import Toucan from "toucan-js";
 import fetchSensibo from "./functions/sensibo/fetchSensibo";
 import fetchTomorrow from "./functions/tomorrow/fetchTomorrow";
 import handleAcState from "./functions/sensibo/handleAcState";
+import handleInteraction from "./functions/discord/handleInteraction";
 
 export default {
-  fetch (
+  async fetch (
     request: Request, env: Env, context: Context,
-  ): Response {
+  ): Promise<Response> {
     const sentry = new Toucan({
       dsn: "https://25641ba90b33429baa4c313eaa8d57d5@o79685.ingest.sentry.io/6397906",
       context,
@@ -18,12 +19,8 @@ export default {
       allowedSearchParams: /(.*)/u,
     },);
     try {
-      const url = new URL(request.url,);
-      if (url.pathname === "/") {
-        return new Response("Hello at root",);
-      } else {
-        return new Response("Hello Else",);
-      }
+      const response = await handleInteraction(request, env,);
+      return response;
     } catch (err) {
       sentry.captureException(err,);
       console.error(err,);
