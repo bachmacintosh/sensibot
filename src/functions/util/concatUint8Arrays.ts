9a8b18c5578faf8b751a993654b04328e59769a4
Collin Bachman <collin@bachman.io>
2022-06-02 16:23:36 -0400

    Remove functions/deps no longer needed

--- src/functions/util/concatUint8Arrays.ts
+++
@@ -1,9 +0,0 @@
-export default function concatUint8Arrays (
-  arr1: Uint8Array,
-  arr2: Uint8Array,
-): Uint8Array {
-  const merged = new Uint8Array(arr1.length + arr2.length,);
-  merged.set(arr1,);
-  merged.set(arr2, arr1.length,);
-  return merged;
-}
